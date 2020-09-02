df1c72a61f228843a81f535fabdcf37ccd0f5073
souravpal32 <souravpal0302@gmail.com>
2020-09-02 12:21:33 +0530

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::latest()->paginate(5);
+  
+        return view('products.index',compact('products'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+  
+        // Product::create($request->all());
+        $product = new product;
+        $product->product_name = $request->name;
+        $product->product_detail = $request->detail;
+        $product->save();
+
+   
+        return redirect('newproducts')
+                        ->with('success','Product created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return view('products.show',compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Student registration
+                </div>
+
+                <div>
+                   <table border='0' width='100%' bgcolor='f2f2f2'>
+                   <tr>
+                   <td align='left' style="color:red" width='50'>
+                   </td>
+                   <td align='left' style="color:red" height='10'>
+                   </td>
+                   <td align='left'>
+                   </td>
+                   </tr>
+                   
+                   <tr>
+                   <td align='left' style="color:red" width='50'>
+                   </td>
+                   <td align='left' style="color:red">
+Name :
+                   </td>
+                   <td align='left'>
+                   <input  type="text" id="name" name="name">
+                   </td>
+
+                   </tr>
+                   <tr>
+                   <td align='left' style="color:red" width='50'>
+                   </td>
+                   <td align='left'>
+City/Town :
+                   </td>
+                   <td align='left'>
+                   <input type="text" id="city" name="city">
+                   </td>
+
+                   </tr>
+                   <tr>
+                   <td align='left' style="color:red" width='50'>
+                   </td>
+                   <td align='left'>
+PIN :
+                   </td>
+                   <td align='left'>
+                   <input type="text" id="pin" name="pin">
+                   </td>
+
+                   </tr>
+                   <tr>
+                   <td align='left' style="color:red" width='50'>
+                   </td>
+                   <td align='left'>
+                   Phone No :
+                   </td>
+                   <td align='left'>
+                   <input type="text"id="phone" name="phone">
+                   </td>
+                   </tr>
+                   <tr>
+                   <td align='left' style="color:red" width='50'>
+                   </td>
+                   <td align='left'>
+                   Email Id :
+                   </td>
+                   <td align='left'>
+                   <input type="text" id="email" name="email">
+                   </td>
+                   </tr>
+                   <tr>
+                   <td align='left' style="color:red" width='50'>
+                   </td>
+                   <td align='left'>
+                   
+                   </td>
+                   <td align='left' height='50'>
+                   <button type="button">Submit</button>
+                   </td>
+                   </tr>
+                   </table>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
